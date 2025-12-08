76d815f97c21c2908f88b17cf1ea8b9fdd101e4b
paulgrammer <paulmugaya@live.com>
2025-12-08 12:53:23 +0100

    chore: initialize account

---
+++ src/account/KcPage.tsx
@@ -0,0 +1,35 @@
+import { Suspense, lazy } from "react";
+import type { ClassKey } from "keycloakify/account";
+import type { KcContext } from "./KcContext";
+import { useI18n } from "./i18n";
+import DefaultPage from "keycloakify/account/DefaultPage";
+import Template from "./Template";
+
+const Password = lazy(() => import("./pages/Password"));
+
+export default function KcPage(props: { kcContext: KcContext }) {
+	const { kcContext } = props;
+
+	const { i18n } = useI18n({ kcContext });
+
+		return (
+			<Suspense>
+				{(() => {
+					switch (kcContext.pageId) {
+						case "password.ftl":
+							return (
+								<Password
+									{...{ kcContext, i18n, classes }}
+									Template={Template}
+									doUseDefaultCss={true}
+								/>
+							);
+						default:
+							return <DefaultPage kcContext={kcContext} i18n={i18n} classes={classes} Template={Template} doUseDefaultCss={true} />;
+					}
+				})()}
+			</Suspense>
+		);
+}
+
+const classes = {} satisfies { [key in ClassKey]?: string };
